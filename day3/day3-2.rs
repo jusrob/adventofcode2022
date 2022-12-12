3736fc33d3af2738c2dd76110326a69a4a7bc39b
jusrob <jrob48@gmail.com>
2022-12-12 17:32:42 -0500

    solving 3-2

--- day3/day3-2.rs
+++ day3/day3-2.rs
@@ -5,29 +5,38 @@ use std::path::Path;
 const PRIORITY: [char; 52] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 fn main() {
+    let mut line_count = 0;
     let mut total_score = 0;
+    let mut group = Vec::new();
+    let mut group_num = 1;
+    let mut found = false;
     let Ok(lines) = read_lines("./input.txt") else { return };
     // Consumes the iterator, returns an (Optional) String
     for line in lines {
+        if line_count == 0 {
+            println!("Group {}", group_num);
+        }
         if let Ok(items) = line {
-            let mut found_item = false;
-            let length = items.len();
-            let half = length/2;
-            let (comp1, comp2) = items.split_at(half);
-            // println!("{}", items);
-            for item in comp1.chars() {
-                // print!("{}", item);
-                if comp2.contains(item) && found_item == false {
-                    found_item = true;
+            println!("{}", items);
+            group.push(items);
+            line_count = line_count + 1;
+        }
+        if line_count == 3 {
+            group_num = group_num + 1;
+            for item in group[0].chars() {
+                if group[1].contains(item) && group[2].contains(item) && found == false {
+                    found = true;
                     let priority_value = PRIORITY.iter().position(|&x| x == item).unwrap() + 1;
-                    println!("{} - {} - {} - {}", comp1, comp2, item, priority_value);
                     total_score = total_score + priority_value;
-                }
+                    println!("{} - {}", item, priority_value);
+                }         
             }
-            // println!(" ");
+            line_count = 0;
+            found = false;
+            group = Vec::new();
         }
     }
-    println!("Total Score - {}", total_score);
+    println!("Total Score {}", total_score);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
