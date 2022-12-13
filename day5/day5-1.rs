78ece833f345f9d0a4c7415163b37d4a97bbc538
jusrob <jrob48@gmail.com>
2022-12-12 21:52:38 -0500

    initalize day5

---
+++ day5/day5-1.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let Ok(lines) = read_lines("./input.txt") else { return };
+    // Consumes the iterator, returns an (Optional) String
+    for line in lines {
+        if let Ok(crates) = line {
+            println!("{}", crates);
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
