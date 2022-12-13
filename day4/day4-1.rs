426a146bf892bff163da531fbc5e91b5fb13e5e6
jusrob <jrob48@gmail.com>
2022-12-12 21:02:17 -0500

    day 4-1 sol

--- day4/day4-1.rs
+++ day4/day4-1.rs
@@ -4,14 +4,29 @@ use std::path::Path;
 
 
 fn main() {
+    let mut matches = 0;
     let Ok(lines) = read_lines("./input.txt") else { return };
     // Consumes the iterator, returns an (Optional) String
     for line in lines {
         if let Ok(pairs) = line {
-            println!("{}", pairs);
+            let (left, right) = pairs.split_once(",").unwrap();
+            let (left_min, left_max) = left.split_once("-").unwrap();
+            let (right_min, right_max) = right.split_once("-").unwrap();
+            if int(left_min) <= int(right_min) && int(left_max) >= int(right_max){
+                    matches = matches + 1;
+            } else if int(right_min) <= int(left_min) && int(right_max) >= int(left_max) {
+                matches = matches + 1;
+            }
         }
     }
+    println!("{} Matches", matches);
 }
+fn int(string: &str) -> i32{
+    return string.parse::<i32>().unwrap()
+}
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
